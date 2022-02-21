d96dc14feeae54cb421df369b7dc5f9c129f0be5
amazjing <amazjing@163.com>
2022-02-21 16:24:43 +0800

    next p:35

--- Seckill/src/com/atguigu/JedisPoolUtil.java
+++ Seckill/src/com/atguigu/JedisPoolUtil.java
@@ -3,35 +3,65 @@ package com.atguigu;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class JedisPoolUtil {
-	private static volatile JedisPool jedisPool = null;
+    private static volatile JedisPool jedisPool = null;
 
-	private JedisPoolUtil() {
-	}
+    private JedisPoolUtil() {
+    }
 
-	public static JedisPool getJedisPoolInstance() {
-		if (null == jedisPool) {
-			synchronized (JedisPoolUtil.class) {
-				if (null == jedisPool) {
-					JedisPoolConfig poolConfig = new JedisPoolConfig();
-					poolConfig.setMaxTotal(200);
-					poolConfig.setMaxIdle(32);
-					poolConfig.setMaxWaitMillis(100*1000);
-					poolConfig.setBlockWhenExhausted(true);
-					poolConfig.setTestOnBorrow(true);  // ping  PONG
-				 
-					jedisPool = new JedisPool(poolConfig, "192.168.218.131", 6379, 60000 );
-				}
-			}
-		}
-		return jedisPool;
-	}
+    public static JedisPool getJedisPoolInstance() {
+        if (null == jedisPool) {
+            synchronized (JedisPoolUtil.class) {
+                if (null == jedisPool) {
+                    JedisPoolConfig poolConfig = new JedisPoolConfig();
+                    poolConfig.setMaxTotal(200);
+                    poolConfig.setMaxIdle(32);
+                    poolConfig.setMaxWaitMillis(100 * 1000);
+                    poolConfig.setBlockWhenExhausted(true);
+                    poolConfig.setTestOnBorrow(true);  // ping  PONG
 
-	public static void release(JedisPool jedisPool, Jedis jedis) {
-		if (null != jedis) {
-			jedisPool.returnResource(jedis);
-		}
-	}
+                    jedisPool = new JedisPool(poolConfig, "192.168.218.131", 6379, 60000);
+                }
+            }
+        }
+        return jedisPool;
+    }
 
+    public static void release(JedisPool jedisPool, Jedis jedis) {
+        if (null != jedis) {
+            jedisPool.returnResource(jedis);
+        }
+    }
+
+    /**
+     * 主从复制
+     */
+
+    private static JedisSentinelPool jedisSentinelPool = null;
+
+    public static Jedis getJedisFromSentinel() {
+        if (jedisSentinelPool == null) {
+            Set<String> sentinelSet = new HashSet<>();
+            //哨兵模式
+            sentinelSet.add("192.168.11.103:26379");
+
+            JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+            jedisPoolConfig.setMaxTotal(10); //最大可用连接数
+            jedisPoolConfig.setMaxIdle(5); //最大闲置连接数
+            jedisPoolConfig.setMinIdle(5); //最小闲置连接数
+            jedisPoolConfig.setBlockWhenExhausted(true); //连接耗尽是否等待
+            jedisPoolConfig.setMaxWaitMillis(2000); //等待时间
+            jedisPoolConfig.setTestOnBorrow(true); //取连接的时候进行一下测试 ping pong
+
+            jedisSentinelPool = new JedisSentinelPool("mymaster", sentinelSet, jedisPoolConfig);
+            return jedisSentinelPool.getResource();
+        } else {
+            return jedisSentinelPool.getResource();
+        }
+    }
 }
